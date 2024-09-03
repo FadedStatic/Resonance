1dd9ef0410a84340019fa89b65b3d675e440226b
Verba de Quibus <static.faded.cpp@gmail.com>
2024-09-03 00:59:44 -0400

    Push multi opt

--- Resonance/menu/commands/player_commands.hpp
+++ Resonance/menu/commands/player_commands.hpp
@@ -3,16 +3,20 @@
 
 std::shared_ptr<cat_menu_option_t> create_player_namespace()
 {
-    std::unordered_map<std::string, std::tuple<men_ty, std::function<menu_option_callback>, std::function<menu_option_callback>, bool>> player_callbacks =
+    std::unordered_map<std::string, std::tuple<men_ty, std::function<menu_option_callback>, std::function<menu_option_callback>, bool, std::vector<std::pair<std::string,std::string>>>> player_callbacks =
     {
-        { "test", {men_ty::reg, [](const std::shared_ptr<menu_option_t>& ctx) -> bool {std::cout << "test1\n"; return true; }, nullptr, false} },
-        { "test2", {men_ty::reg, [](const std::shared_ptr<menu_option_t>& ctx) -> bool {std::cout << "test2\n"; return true; }, nullptr, false} },
-        {"Godmode", {men_ty::check, [](const std::shared_ptr<menu_option_t>& ctx) -> bool {std::cout << "Godmode!\r\n"; ENTITY::SET_ENTITY_INVINCIBLE(PLAYER::GET_PLAYER_PED(-1), true); return true; }, [](const std::shared_ptr<menu_option_t>& ctx) -> bool {ENTITY::SET_ENTITY_INVINCIBLE(PLAYER::GET_PLAYER_PED(-1), false); return true; }, false}}
+        { "test", {men_ty::reg, [](const std::shared_ptr<menu_option_t>& ctx) -> bool {std::cout << "test1\n"; return true; }, nullptr, false, {}}},
+        { "test2", {men_ty::reg, [](const std::shared_ptr<menu_option_t>& ctx) -> bool {std::cout << "test2\n"; return true; }, nullptr, false, {}}},
+        {"Godmode", {men_ty::check, [](const std::shared_ptr<menu_option_t>& ctx) -> bool {std::cout << "Godmode!\r\n"; return true;  }, [](const std::shared_ptr<menu_option_t>& ctx) -> bool { return true; }, false, {}}},
+        { "Wanted Level", {men_ty::multi, [](const std::shared_ptr<menu_option_t>& ctx) -> bool { return true; }, nullptr, false,{
+                {"Poop", "Deck"},
+                {"Fuckus", "Niggus"},
+            } } }
     };
 
     auto ret = std::make_shared<cat_menu_option_t>("Player");
     for (const auto& [name, pr] : player_callbacks) {
-        switch (const auto& [ty, fn, cb, def_en] = pr; ty)
+        switch (const auto& [ty, fn, cb, def_en, opts] = pr; ty)
         {
             case men_ty::check:
                 ret->options.emplace_back(
@@ -22,6 +26,11 @@ std::shared_ptr<cat_menu_option_t> create_player_namespace()
             case men_ty::reg:
                 ret->options.emplace_back(
                     std::make_shared<menu_option_t>(name, fn)
+                );
+            break;
+            case men_ty::multi:
+                ret->options.emplace_back(
+                    std::make_shared<multi_option_t>(name, opts)
                 );
             break;
         }
